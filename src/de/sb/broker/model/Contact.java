297b1eb8a9f1a9b59c9e4d64873cd20811a8e999
Timo N <timo.neumann@student.htw-berlin.de>
2016-10-20 21:39:01 +0200

    Included some more eclipse-project files in .gitignore, added getters/setters to Contact.java and Name.java

--- src/de/sb/broker/model/Contact.java
+++ src/de/sb/broker/model/Contact.java
@@ -4,5 +4,23 @@ public class Contact {
 	private String email;
 	private String phone;
 	
+	public Contact(String email, String phone) {
+		this.email = email;
+		this.phone = phone;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return this.phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
 	
 }
--- src/de/sb/broker/model/Name.java
+++ src/de/sb/broker/model/Name.java
@@ -3,5 +3,23 @@ package de.sb.broker.model;
 public class Name {
 	private String family;
 	private String given;
+	
+	public Name(String family, String given) {
+		this.family = family;
+		this.given = given;
+	}
+	
+	public String getFamily() {
+		return this.family;
+	}
+	public void setFamily(String family) {
+		this.family = family;
+	}
+	public String getGiven() {
+		return this.given;
+	}
+	public void setGiven(String given) {
+		this.given = given;
+	}
 
 }
