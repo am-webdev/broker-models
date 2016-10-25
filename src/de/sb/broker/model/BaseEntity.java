e5a6145e471e990f7a7db3b31bb0a76731090924
Andreas Morgner <amorgner@Andreass-MacBook-Air.local>
2016-10-25 12:27:25 +0200

    JPA mapping for BaseEntity

--- src/de/sb/broker/model/BaseEntity.java
+++ src/de/sb/broker/model/BaseEntity.java
@@ -1,14 +1,31 @@
 package de.sb.broker.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Table;
 
+@Entity
 @Table(name="BaseEntity", schema="_s0545840__brokerDB")
 public class BaseEntity implements Comparable<BaseEntity> {
 
+	/*   Attributes   */
+	/* ************** */
+
+	@Column(name = "identity", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long identity;
+	
+	@Column(name = "version")
 	private int version;
+	
+	@Column(name = "creationTimestamp")
 	private long creationTimeStamp;
 
+	/*   Methods   */
+	/* *********** */
+	
 	@Override
 	public int compareTo(BaseEntity o) {
 		return Long.compare(this.identity, o.identity);
