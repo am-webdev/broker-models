1310cc67ffddba659bbedfdea627eef7a9390696
Thilo Ilg <contact@thiloilg.com>
2016-11-22 18:16:48 +0100

    auction put methods splitted in create and update

--- src/de/sb/broker/rest/AuctionService.java
+++ src/de/sb/broker/rest/AuctionService.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import javax.validation.Valid;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -54,33 +55,48 @@ public class AuctionService {
 	 */
 	@PUT
 	@Produces(MediaType.APPLICATION_XML)
-	public void setAuction(Auction a){
+	public void createAuction(@Valid Auction a){
 		final EntityManager em = emf.createEntityManager();
 		try{
-			if(a.getIdentity() == 0){ // creates new auction
-				em.getTransaction().begin();
-				em.persist(a);
-				em.getTransaction().commit();
-			}else{
-				em.getTransaction().begin();
-				Auction toUpdate = em.find(Auction.class, a.getIdentity());
-				if(!toUpdate.isClosed() && toUpdate.getBids().size() <= 0){ // update auction
-					if(a.getAskingPrice() != 0) toUpdate.setAskingPrice(a.getAskingPrice());
-					if(a.getClosureTimestamp() != 0) toUpdate.setClosureTimestamp(a.getClosureTimestamp());
-					if(a.getDescription() != null) toUpdate.setDescription(a.getDescription());
-					if(a.getSeller() != null) toUpdate.setSeller(a.getSeller());
-					if(a.getTitle() != null) toUpdate.setTitle(a.getTitle());
-					if(a.getUnitCount() != 0)toUpdate.setUnitCount(a.getUnitCount());
-					if(a.getVersion() != 0)toUpdate.setVersion(a.getVersion());
-				}
-				em.getTransaction().commit();
-			}
+			em.getTransaction().begin();
+			em.persist(a);
+			em.getTransaction().commit();
 		}finally{
 	        if(em.getTransaction().isActive()){
 	            System.out.println("Entity Manager Rollback");
 	            em.getTransaction().rollback();
 	        }   
-	        em.clear();
+	        em.close();
+		}
+	}
+	
+	/**
+	 * Creates or modifies an auction from the given template data. Note
+	 * that an auction may only be modified as long as it is not sealed (i.e. is open and still
+	 * without bids).
+	 */
+	@PUT
+	@Produces(MediaType.APPLICATION_XML)
+	public void updateAuction(@Valid Auction a){
+		final EntityManager em = emf.createEntityManager();
+		try{
+			em.getTransaction().begin();
+			Auction toUpdate = em.find(Auction.class, a.getIdentity());
+			if(!toUpdate.isClosed() && toUpdate.getBids().size() <= 0){ // update auction
+				if(a.getAskingPrice() != 0) toUpdate.setAskingPrice(a.getAskingPrice());
+				if(a.getClosureTimestamp() != 0) toUpdate.setClosureTimestamp(a.getClosureTimestamp());
+				if(a.getDescription() != null) toUpdate.setDescription(a.getDescription());
+				if(a.getSeller() != null) toUpdate.setSeller(a.getSeller());
+				if(a.getTitle() != null) toUpdate.setTitle(a.getTitle());
+				if(a.getUnitCount() != 0)toUpdate.setUnitCount(a.getUnitCount());
+				if(a.getVersion() != 0)toUpdate.setVersion(a.getVersion());
+			}
+			em.getTransaction().commit();
+		}finally{
+	        if(em.getTransaction().isActive()){
+	            System.out.println("Entity Manager Rollback");
+	            em.getTransaction().rollback();
+	        }
 	        em.close();
 		}
 	}
